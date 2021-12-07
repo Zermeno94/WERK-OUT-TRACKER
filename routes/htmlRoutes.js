f834a3d6d5047128dad85be69169fd00c56a106d
MIRANDA <miranda.zermeno4@gmail.com>
2021-12-06 22:02:02 -0600

    cont added todo comments to html routes in routes folder

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,1 +1,6 @@
 const path = require("path");
+
+//TODO
+// Need to create a path to public files 
+//paths to consider is excercise since we want the excercise date to be displayed
+//also include stats in publics as well
