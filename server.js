419e81049b02b0f8d1925866eef7a9b41a7d5b32
MIRANDA <miranda.zermeno4@gmail.com>
2021-12-06 21:48:02 -0600

    adjusted starter code

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { // changed it to the workout instead of budget
   useNewUrlParser: true,
   useFindAndModify: false
 });
