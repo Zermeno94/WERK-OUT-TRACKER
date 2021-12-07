6a97884a4a897b0e796185278396c805f5bebe6d
MIRANDA <miranda.zermeno4@gmail.com>
2021-12-06 22:14:47 -0600

    added starter code to workout.js in models folder and TODO comments

---
+++ models/workout.js
@@ -0,0 +1,27 @@
+//depencies 
+const mongoose =require("mongoose");
+const Schema = mongoose.Schema;
+
+//Starter code from mini project and rename to 'workout'
+const workoutSchema = new Schema({
+    name: { 
+      type: String,
+      trim: true,
+      required: "Enter a name for transaction"
+    },
+    value: {
+      type: Number,
+      required: "Enter an amount"
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  });
+  
+  const workout = mongoose.model("Workout", workoutSchema);
+  
+  module.exports = workout;
+
+  //TODO
+// Need to adjust the schem to reflect day,excercises (name,sets,etc)
