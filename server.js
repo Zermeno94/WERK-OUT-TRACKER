4a4799251493b6ba624e24637a612bed860e1e85
MIRANDA <miranda.zermeno4@gmail.com>
2021-12-06 21:49:47 -0600

    added todo comments to server.js

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { // 
 
 // routes
 app.use(require("./routes/api.js"));
+// app.use(require("./routes/")) need to link up to html?
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
