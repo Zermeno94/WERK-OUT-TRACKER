bb256635afeed6a1501837643876f499351fe8cd
MIRANDA <miranda.zermeno4@gmail.com>
2021-12-06 22:37:37 -0600

    added get method scode to api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,20 @@
 const db = require("../models");
+const router =require("express").Router();
 
+router.get("/api/workouts",(req,res)=> {
+    db.workout.find({})
+    .thn(dbWorkout =>{
+        res.json(dbWorkout);
+    })
+})
+
+// INSERT POST METHOD HERE 
+
+// INSERT PUT METHOD HERE
+
+module.exports =router;
 //TODO
 // Need to pull data for the workout display page
 // Need to POST completed workouts 
 //Need to UPDATE works via MongoDB 
+// Double check syntax to other files to avoid errors
