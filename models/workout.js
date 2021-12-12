b83197788b20d03dda3e5ebb57e79ca7fee6bf81
MIRANDA <miranda.zermeno4@gmail.com>
2021-12-11 22:20:46 -0600

    added excises types to workout.js

--- models/workout.js
+++ models/workout.js
@@ -4,19 +4,43 @@ const Schema = mongoose.Schema;
 
 //Starter code from mini project and rename to 'workout'
 const workoutSchema = new Schema({
-    name: { 
-      type: String,
-      trim: true,
-      required: "Enter a name for transaction"
-    },
-    value: {
-      type: Number,
-      required: "Enter an amount"
-    },
-    date: {
-      type: Date,
-      default: Date.now
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+      },
+      name: {
+        type: String,
+        trim: true,
+      },
+      duration: Number,
+      weight: {
+        type: Number,
+        default: 0
+      },
+      reps: {
+        type: Number,
+        default: 0
+      },
+      sets: {
+        type: Number,
+        default: 0
+      },
+      distance: {
+        type: Number,
+        default: 0
+      }
     }
+  ],
+  totalDuration: {
+    type: Number,
+    default: 0,
+  }
   });
   
   const workout = mongoose.model("Workout", workoutSchema);
