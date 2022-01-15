665c187682c505ec18d6bed1943da1726bf8a458
MIRANDA <miranda.zermeno4@gmail.com>
2022-01-14 19:42:32 -0600

    updated todo notes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,6 @@
 const path = require("path");
 const router = require("express").Router();
 
-//Pulled code from past activities 
-// router.get("/", (req,res)=> {
-//     res.sendFile(path.join(__dirname, "index.html"))
-// })
 
 router.get("/excercise", (req,res)=> {
     res.sendFile(path.join(__dirname, "../public/excercise.html"));
