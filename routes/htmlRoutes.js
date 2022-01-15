598dd0d10c99c4767bc51e1ee0044d55cbd8b5a3
MIRANDA <miranda.zermeno4@gmail.com>
2022-01-14 20:16:05 -0600

    updated todo notes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -15,8 +15,3 @@ router.get('/', (req, res) => {
   });
 
 module.exports = router;
-
-//TODO
-// Need to create a path to public files 
-//paths to consider is excercise since we want the excercise date to be displayed
-//also include stats in publics as well
