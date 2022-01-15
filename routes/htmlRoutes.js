aaf4b3cf5ac7250c5e74413185be863c7a4b889c
MIRANDA <miranda.zermeno4@gmail.com>
2022-01-14 21:02:05 -0600

    adj paths in html rute due to browser error

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,18 +1,19 @@
 const path = require("path");
 const router = require("express").Router();
 
-
-require("../models");
-router.get("/exercise", (req,res)=> {
-    res.sendFile(path.join(__dirname, ".../public/exercise.html"));
-});
-
-router.get("/stats",(req,res)=>{
-    res.sendFile(path.join(__dirname, "../public/stats.html"));
-});
-
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
+  
 
-module.exports = router;
+  router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+  
+
+  router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+  
+
+  module.exports = router;
